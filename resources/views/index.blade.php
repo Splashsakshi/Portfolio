465f24140bf09e3d03cbbbd9cbe675fcb2680766
SakshiDev008 <sakshisharma57543@gmail.com>
2024-04-01 18:33:13 -0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,531 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	{{-- <link rel="icon" href="img/favicon.png" type="image/png"> --}}
+	<title>SS Portfolio</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+{{-- <style>
+    
+    #container {
+  width: 200px;
+  height: 300px;
+  border-radius: 12px;
+  overflow: hidden;
+}
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+  transform: scale(1.3);
+  filter: grayscale(1);
+  transition: 400ms ease;
+}
+
+#container:hover > .image {
+  filter: grayscale(0);
+  transform: scale(1);
+}
+
+
+    </style> --}}
+	<style>
+		.home_right_img::::before{
+
+		}
+
+  #div1{
+ 
+  
+  font-weight: bold;
+  position: relative;
+  animation: mymove 2s;
+  animation-fill-mode: forwards;
+}
+
+#div1 {animation-timing-function: linear;}
+
+
+@keyframes mymove {
+  from {left: 0px;}
+  to {left: 100px;}
+}
+  #div2{
+ 
+  
+  font-weight: bold;
+  position: relative;
+  animation: mymove 2s;
+  animation-fill-mode: backwards;
+}
+
+#div2 {animation-timing-function: linear;}
+
+
+@keyframes mymove {
+  from {left: 0px;}
+  to {right: 100px;}
+}
+
+#footer_logo h1{
+	
+  -webkit-box-reflect: right;
+}
+
+
+		</style>
+
+
+<body>
+	<!--================ Start Header Area =================-->
+	<div class="running-text">
+		<marquee direction="right" class="text-white">
+			<em><h1>welcome to my journey </h1></em> 
+			</marquee>
+	</div>
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					{{-- <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a> --}}
+                    <h1 class="log">Folio.</h1>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav justify-content-end">
+							<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="#about_us">About</a></li>
+							<li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
+							<li class="nav-item"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="	"
+								 aria-expanded="false">Pages</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+									<li class="nav-item"><a class="nav-link" href="portfolio-details.html">Portfolio Details</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">Blog</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+									<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+								</ul>
+							</li>
+							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================ End Header Area =================-->
+
+	<!--================ Start Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-7">
+						<div class="banner_content">
+							<h3 class="text-uppercase">Hell0</h3>
+							<h1 class=""><i>I am'</i> <span class="multiText" style="
+								color: #28a745;
+								text-transform: capitalize;
+						  
+							  "></span> </h1>
+							<h5 class="text-uppercase"> Software developer</h5>
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#"><span>Hire Me</span></a>
+								<a class="primary_btn tr-bg" href="#"><span>Get CV</span></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-5" >
+					
+						<img class="" src="img/banner/home-right.png" alt="" style="margin-top: 116px; margin-left: 125px;" id="div1">
+						
+						{{-- <div class="box">
+						<div id="pixel"></div>
+						</div> --}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Home Banner Area =================-->
+
+	<!--================ Start About Us Area =================-->
+	<section class="about_area section_gap" id="about_us">
+		<div class="container">
+			<div class="row justify-content-start align-items-center">
+				<div class="col-lg-5">
+					<div class="about_img">
+						<img class="" src="img/about-us.png" alt="" id="div2">
+					</div>
+				</div>
+
+				<div class=" 
+				
+				
+				-lg-1 col-lg-5">
+					<div class="main_title text-left">
+						<h2>let’s <br>
+							Introduce about <br>
+							myself</h2>
+						<p>
+							Greetings! I Am Sakshi and I am delighted to welcome you to my portfolio. As a Software Developer, I bring  1Year of experience and a passion for Database server.
+						</p>
+						<p>
+							In my professional journey, I have done goverment projects like <i>Nagpur Mahametro, Mahagenco.</i> 
+                            
+						</p>
+						<a class="primary_btn" href="#"><span>Download CV</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End About Us Area =================-->
+
+	<!--================ Srart Brand Area =================-->
+	<section class="brand_area section_gap_bottom">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo1.png" alt=""> --}}
+                                    <h3>HTML</h3>   
+								</div>
+							</div>
+						</div>
+						{{-- <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+                                    <div id="container">
+                                        <img src="img/brands/R.png" alt="" class="image">
+                                      </div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+                                    <div id="container">
+                                        <img src="img/brands/css.jpg" alt="" class="image">
+                                      </div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+                                    <div id="container">
+                                        <img src="img/brands/bootstrap.png" alt="" class="image">
+                                      </div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+                                    <div id="container">
+                                        <img src="https://e7.pngegg.com/pngimages/662/163/png-clipart-jquery-logo-web-development-jquery-ui-javascript-computer-icons-jqlogo-emblem-label.png" alt="" class="image">
+                                      </div>
+								</div>
+							</div>
+						</div> --}}
+                        
+
+						 <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo3.png" alt=""> --}}
+                                    <h3>CSS</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo4.png" alt=""> --}}
+                                    <h3>Bootstrap</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo5.png" alt=""> --}}
+                                    <h3>jquery</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo6.png" alt=""> --}}
+                                    <h3>PHP</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo7.png" alt=""> --}}
+                                    <h3>Laravel</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo8.png" alt=""> --}}
+                                    <h3>MySql</h3>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo9.png" alt=""> --}}
+                                    <h3>Flutter</h3>
+								</div>
+							</div>
+						</div> 
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo9.png" alt=""> --}}
+                                    <h3>ASP.Net</h3>
+								</div>
+							</div>
+						</div> 
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="single-brand-item d-table">
+								<div class="d-table-cell text-center">
+									{{-- <img src="img/brands/logo9.png" alt=""> --}}
+                                    <h3>Sql</h3>
+								</div>
+							</div>
+						</div> 
+					</div>
+                    {{-- <div id="container">
+                        <img src="img/brands/R.png" alt="" class="image">
+                      </div> --}}
+				</div>
+               
+				<div class="offset-lg-2 col-lg-4 col-md-6">
+					<div class="client-info">
+						<div class="d-flex mb-50">
+							<span class="lage">1</span>
+							<span class="smll">Year+ Experience Working</span>
+						</div>
+						<div class="call-now d-flex">
+							<div>
+								<span class="fa fa-phone"></span>
+							</div>
+							<div class="ml-20">
+								<p>Contact on Mail</p>
+								<h6>sakshisharma57543@gmail.com</h6>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Brand Area =================-->
+
+	<!--================ Start Features Area =================-->
+	<section class="features_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2> service </h2>
+						<p>
+							Is give may shall likeness made yielding spirit a itself togeth created 
+							after sea <br> is in beast beginning signs open god you're gathering ithe
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row feature_inner">
+				<div class="col-lg-4 col-md-6">
+					<div class="feature_item">
+						<img src="img/services/s1.png" alt="">
+						<h4 style="color: black">Web developing</h4>
+						<p>The coding and programming side of website production. When you learn web development, you might start out writing a simple page of HTML text and build up to creating more complex.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="feature_item">
+						<img src="img/services/s2.png" alt="">
+						<h4 style="color: black">UI/ux design</h4>
+						<p>UX design focuses on the interaction that a human user has with everyday products and services.The goal of UX design is to make using these products and services, both digital or physical, easy,logical,and fun.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="feature_item">
+						<img src="img/services/s3.png" alt="">
+						<h4 style="color: black">Web design</h4>
+						<p>   The three most common types of web design are static web design, dynamic web design, and eCommerce web design.   </p>
+						<br><br>
+					</div>
+				</div>
+				{{-- <div class="col-lg-3 col-md-6">
+					<div class="feature_item">
+						<img src="img/services/s4.png" alt="">
+						<h4>seo optimize</h4>
+						<p>Creeping for female light years that lesser can't evening heaven isn't bearing tree</p>
+					</div>
+				</div> --}}
+			</div>
+		</div>
+	</section>
+	<!--================ End Features Area =================-->
+
+	<!--================Start Portfolio Area =================-->
+
+	<!--================End Portfolio Area =================-->
+
+	<!--================ Start Testimonial Area =================-->
+
+	<!--================ End Testimonial Area =================-->
+
+	<!--================ Start Newsletter Area =================-->
+	{{-- <section class="newsletter_area">
+		<div class="container">
+			<div class="row justify-content-center align-items-center">
+				<div class="col-lg-12">
+					<div class="subscription_box text-center">
+						<h2 class="text-uppercase text-white">get update from anywhere</h2>
+						<p class="text-white">
+							Bearing Void gathering light light his eavening unto dont afraid. 
+						</p>
+						<div class="subcribe-form" id="mc_embed_signup">
+							<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
+								<input name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required="" type="email">
+								<div style="position: absolute; left: -5000px;">
+									<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+								</div>
+								<button class="primary-btn hover d-inline">Get Started</button>
+								<div class="info"></div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section> --}}
+	<!--================ End Newsletter Area =================-->
+
+	<!--================Footer Area =================-->
+	<footer class="footer_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-12">
+					<div class="footer_top flex-column">
+						<div class="footer_logo">
+							<a href="#">
+								{{-- <img src="img/logo.png" alt=""> --}}
+								
+								<h1><i>Folio</i></h1>
+							</a>
+							<h4 style="color: green">Follow Me</h4>
+						</div>
+						<div class="footer_social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-instagram"></i></a>
+							<a href="#"><i class="fa fa-linkedin"></i></a>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row footer_bottom justify-content-center">
+				<p class="col-lg-8 col-sm-12 footer-text">
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank"></a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+			</div>
+		</div>
+	</footer>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="vendors/isotope/isotope-min.js"></script>
+	<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="js/gmaps.min.js"></script>
+	<script src="js/theme.js"></script>
+	<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+	<script>
+		var typingEffect = new Typed(".multiText", {
+			strings :["Sakshi Sharma","software Devloper"],
+			loop : true,
+			typeSpeed : 100,
+			backDelay : 1500
+		})
+		</script>
+
+		{{-- <script>
+			let W = 10;
+			let h = 10;
+
+			function pixel(){
+				for(var i=0; i<h; i++){
+					for(var j= 0; j<W; j++){
+						let span = document.createElement('span');
+						let random = Math.random()*5;
+						let randomFixed = random.toFixed(2)
+
+						document.getElementById('pixel').appendChild(span);
+						span.style.left = j * 10 + 'px';
+						span.style.top = i *10 + 'px';
+						span.style.backgroundPosition = -j*10 +'px ' + -i*10 + 'px, center' ;
+						span.style.animationDelay = randomFixed + 's';
+					}
+				}
+			}
+			pixel();
+			</script> --}}
+</body>
+
+</html>
