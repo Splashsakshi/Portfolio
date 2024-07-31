7d74cfdaf7ef8baa733e0d24cd16414466ac10b2
Splashsakshi <164610137+Splashsakshi@users.noreply.github.com>
2024-07-31 12:13:45 +0530

    Update mail.blade.php

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,11 +1,11 @@
 <div class="col-sm-4">
     <p>sakshi shij  ji </p>
-    <h2>csdsd</h2>
+    <h3>csdsd</h3>
 
 </div>
 
 <div class="col-sm-6">
-    <p>bhdjcbsdc</p>
+    <h2>bhdjcbsdc</h2>
 </div>
 
 
